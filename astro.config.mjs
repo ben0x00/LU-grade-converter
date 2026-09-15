2277aa099ea168428f4c33a878ac6d36d39d5218
Ben Toon <bentoon00@gmail.com>
2026-09-15 01:10:25 +0100

    Update config for pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://ben0x00.github.io/LU-grade-converter/',
+  base: '/LU-grade-converter'
+});
